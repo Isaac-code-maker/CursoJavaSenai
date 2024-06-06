ff68f2c3e1b5c63b7082d1e6293dbd37c682da5a
IsaacTh <83322345+Isaac-code-maker@users.noreply.github.com>
2024-06-06 17:30:49 -0300

    Aula tipos variaveis e condicionais

---
+++ ProgramaJava/src/App.java
@@ -0,0 +1,36 @@
+public class App {
+    public static void main(String[] args){
+
+        // double n1 = 3.3;
+        // double n2 = 6;
+        // double n3 = 7.7;
+
+        // double media;
+        // media = (n1 + n2 + n3)/3;
+
+        // System.out.printf("%.2f", media);
+
+        // int idade = 17;
+
+        // if(idade >= 18){
+
+        //     System.out.println("Usuário maior de idade");
+
+        // }else{
+        //     System.out.println("Usuário menor de idade");
+        // }
+
+        // int n1, n2;
+        
+        // n1 = -5;
+        // n2 = 8;
+
+        // int res = n1*n2;
+
+        // if(res < 0){
+        //     System.out.printf("%d é negativo", res);
+        // }else{
+        //     System.out.printf("%d é positivo", res);
+        // }
+    }
+}
