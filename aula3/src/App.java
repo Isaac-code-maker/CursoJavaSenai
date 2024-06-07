c1a633a3c1a391c5ad2e9ec1a3a0d10fcc6f8b8b
IsaacTh <83322345+Isaac-code-maker@users.noreply.github.com>
2024-06-07 17:48:00 -0300

    content aula 3

---
+++ aula3/src/App.java
@@ -0,0 +1,89 @@
+public class App {
+    public static void main(String[] args){
+
+        // System.out.println("Hello, World!");
+
+        // int n1 = 15;
+
+        // if ((n1 > 0) && (n1 < 100)){
+
+        //     System.out.println("O número está entre 0 e 100");
+
+        // }else{
+        //     System.out.println("O número não está entre 0 e 100");
+        // }
+
+        // if(n1 % 2 != 0){
+
+        //     System.out.printf("O número %d é ímpar", n1);
+
+        // }else{
+
+        //     System.out.printf("O número %d é par", n1);
+
+        // }
+
+        // int i = 0;
+
+        // for(i = 0;i < 10;i++){
+
+        //     System.out.println(i);
+
+        // }
+
+        //  int n1 =0;
+
+        // while(true){
+        //     System.out.println(n1);
+        //     n1++;
+        // }
+
+        // do {
+
+            
+
+        // }while(n1 > 100);
+
+        // int i,j,posi,k,i2;
+
+        // posi = 6;
+        // k = 1;
+
+        // for(i=1; i<=6 ;i++){
+        //     for(j = 1; j <= 10;j++)
+        //         if(j == posi){
+        //             for(i2=1; i2 <= k; i2++)
+        //                 System.out.print('*');
+
+        //             k += 2;
+        //             posi--;
+
+        //         }else
+        //             System.out.print(' ');
+
+        //         System.out.println();
+        // }
+
+        
+        
+        // int[][] mat = new int[10][10];
+        // int i = 0, j = 0;
+
+
+        // for(i=0; i < mat.length; i++){
+        //     for(j=0; j < mat.length; j++){
+
+        //         mat[i][j] = i * j;
+        //     }
+        // }
+
+        // for(i =0; i < mat.length; i++){
+        //     for(j = 0; j < mat.length; j++){
+
+        //         System.out.print(mat[i][j] + " ");
+                
+        //     }
+        // //     System.out.println();
+        // }
+    }
+}
